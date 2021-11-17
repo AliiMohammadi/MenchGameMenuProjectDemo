ac3b29b6e2481d8314643ec78508ca2fbcf13e26
Ali mohammadi <alimohammady0921@gmail.com>
2021-11-17 18:57:37 +0330

    UI,UX , swipe side panels opation

--- Mench Game Menu Demo/Assets/Programms/Scripts/UI/Menu/GameOptionsManager.cs
+++ Mench Game Menu Demo/Assets/Programms/Scripts/UI/Menu/GameOptionsManager.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameOption
 {
+    /// <summary>
+    /// اسکریپت جهت گزاشتن تنظیمات بازی و یا ذخیره تنظیمات جدید.
+    /// این کلاس میتواند بدون محدودیت بر هر سینی اضافه شود
+    /// </summary>
     public class GameOptionsManager : MonoBehaviour
     {
+        [Header("Setting UI elements refrences.")]
+        [SerializeField]
+        private Scrollbar MusicVolumElement;
+        [SerializeField]
+        private Scrollbar SoundEffectsVolumElement;
+
         [HideInInspector]
         public static GameOptionsManager Gamesettings;
 
@@ -19,13 +30,31 @@ namespace GameOption
             Gamesettings = this;
             Refresh();
         }
-
         void LoadSavedSettingData()
         {
             GameOptions = GameSetting.GetSavedSettings();
+            print(GameOptions.SoundEffectsVolum);
             Debug.Log("Setting Data Loaded.");
         }
+        void SetSavedSettingData()
+        {
+            MusicVolumElement.value = GameOptions.MusicVolum;
+            SoundEffectsVolumElement.value = GameOptions.SoundEffectsVolum;
+            //زبان اضافه شود بعدا
+        }
 
+        /// <summary>
+        /// زمانی که فرم مقدار عانصر یو ای تنظیمات عوض میشود این تواببع خوانده شود تا مقدایر جدید 
+        /// اضافه شوند
+        /// </summary>
+        public void UpdateSettings()
+        {
+            GameOptions.MusicVolum = MusicVolumElement.value;
+            GameOptions.SoundEffectsVolum = SoundEffectsVolumElement.value;
+
+            //زبان اضافه شود بعدا
+            SaveCurrentSettingData();
+        }
         /// <summary>
         /// ذخیره داده های حال در حافظه
         /// </summary>
@@ -37,6 +66,7 @@ namespace GameOption
         public void Refresh()
         {
             LoadSavedSettingData();
+            SetSavedSettingData();
         }
     }
 }
